fc40b4a20dce65363b5009fc6bb9a771f8f8d8db
ougueur <ougueur.k@gmail.com>
2022-03-18 22:28:44 +0100

    question 3 tp finished

--- code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
+++ code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
@@ -1,10 +1,37 @@
 package fr.istic.vv;
+import java.util.Comparator;
+
 import net.jqwik.api.*;
+import net.jqwik.engine.providers.IntegerArbitraryProvider;
 
 
 public class BinaryHeapTest {
+
+    Comparator<Integer> comparator = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer leftElement, Integer rightElement) {
+            return leftElement.compareTo(rightElement);
+        }
+    };
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean heapTest(@ForAll("binaryHeap") BinaryHeap<Integer> heap) {
+        if (heap.count() == 0) return true;
+        Integer min = heap.pop();
+        if (comparator.compare(min, min = heap.pop()) > 0) {
+            return false;
+        }
+        return true;
     }
+
+    @Provide
+    Arbitrary<BinaryHeap<Integer>> binaryHeap() {
+        BinaryHeap<Integer> binaryHeap = new BinaryHeap<Integer>(comparator, new Integer[3]);
+        Arbitrary<Integer> ints = Arbitraries.integers().between(1, 2);
+        return Arbitraries.integers().between(0, 3).map(i -> {
+            ints.forEachValue(val -> binaryHeap.push(i));
+            return binaryHeap;
+        });
+    }
+
 }
